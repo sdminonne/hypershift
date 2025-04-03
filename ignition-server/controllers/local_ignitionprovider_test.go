3e5b683491fae69c7f7693a7fdcef72e167e0263
Salvatore Dario Minonne <sminonne@redhat.com>
2025-04-03 17:13:58 +0200

    add unit tests to LocalIgnitionProvider.GetPayload func
    Signed-off-by: Salvatore Dario Minonne <sminonne@redhat.com>

--- ignition-server/controllers/local_ignitionprovider_test.go
+++ ignition-server/controllers/local_ignitionprovider_test.go
@@ -6,9 +6,18 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"reflect"
+	"sync"
 	"testing"
 
 	. "github.com/onsi/gomega"
+	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
+	"github.com/openshift/hypershift/support/releaseinfo"
+	"github.com/openshift/hypershift/support/util"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func TestExtractMCOBinaries(t *testing.T) {
@@ -124,6 +133,243 @@ func TestExtractMCOBinaries(t *testing.T) {
 				fileContent, err := os.ReadFile(filePath)
 				g.Expect(err).NotTo(HaveOccurred())
 				g.Expect(string(fileContent)).To(Equal(tc.expectedBinaryVersion))
+			}
+		})
+	}
+}
+
+type myFakeclient struct {
+	client.WithWatch
+}
+
+func (c myFakeclient) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
+	if key.Namespace == "failed-pull-secret-ns" && key.Name == "pull-secret" {
+		return fmt.Errorf("failed to get pull secret")
+	}
+	if key.Namespace == "missing-dockerconfigjson-key-ns" && key.Name == "pull-secret" {
+		obj = &corev1.Secret{}
+		return nil
+	}
+	if key.Namespace == "pull-secret-does-not-match-ns" && key.Name == "pull-secret" {
+		obj.(*corev1.Secret).Data = map[string][]byte{
+			corev1.DockerConfigJsonKey: []byte("data"),
+		}
+		return nil
+	}
+	if key.Namespace == "no-additional-trust-bundle-ns" {
+		switch key.Name {
+		case "pull-secret":
+			{
+				obj.(*corev1.Secret).Data = map[string][]byte{
+					corev1.DockerConfigJsonKey: []byte("data"),
+				}
+				return nil
+			}
+		case additionalTrustBundleName:
+			{
+				return fmt.Errorf("error")
+			}
+		}
+		return nil
+	}
+	panic("should not be here NS:" + key.Namespace + " Name:" + key.Name + " Kind" + reflect.TypeOf(obj).String())
+	return nil
+}
+
+func TestLocalIgnitionProvider_GetPayload_clientErrors(t *testing.T) {
+	type fields struct {
+		Client                client.Client
+		ReleaseProvider       releaseinfo.ProviderWithOpenShiftImageRegistryOverrides
+		CloudProvider         hyperv1.PlatformType
+		Namespace             string
+		WorkDir               string
+		PreserveOutput        bool
+		FeatureGateManifest   string
+		ImageMetadataProvider *util.RegistryClientImageMetadataProvider
+		ImageFileCache        *imageFileCache
+		lock                  sync.Mutex
+	}
+	type args struct {
+		ctx                       context.Context
+		releaseImage              string
+		customConfig              string
+		pullSecretHash            string
+		additionalTrustBundleHash string
+		hcConfigurationHash       string
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		want      []byte
+		wantError bool
+	}{
+		{name: "Error: failed to get pull secret",
+			fields: fields{
+				Namespace: "failed-pull-secret-ns",
+				lock:      sync.Mutex{},
+			},
+			args: args{
+				ctx: context.TODO(),
+			},
+			want:      nil,
+			wantError: true,
+		},
+		{
+			name: "Error: pull secret missing .dockerconfigjson key",
+			fields: fields{
+				Namespace: "missing-dockerconfigjson-key-ns",
+				lock:      sync.Mutex{},
+			},
+			args:      args{},
+			wantError: true,
+		},
+		{
+			name: "Error:  pull secret does not match",
+			fields: fields{
+				Namespace: "pull-secret-does-not-match-ns",
+			},
+			args: args{
+				pullSecretHash: "wrong-pull-secret-hash",
+				ctx:            context.TODO(),
+			},
+			want:      nil,
+			wantError: true,
+		},
+		{
+			name: "Error: no additional trust bundle",
+			fields: fields{
+				Namespace: "no-additional-trust-bundle-ns",
+			},
+			args: args{
+				ctx: context.TODO(),
+			},
+			want:      nil,
+			wantError: true,
+		},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			p := &LocalIgnitionProvider{
+				Client:                myFakeclient{},
+				ReleaseProvider:       tt.fields.ReleaseProvider,
+				CloudProvider:         tt.fields.CloudProvider,
+				Namespace:             tt.fields.Namespace,
+				WorkDir:               tt.fields.WorkDir,
+				PreserveOutput:        tt.fields.PreserveOutput,
+				FeatureGateManifest:   tt.fields.FeatureGateManifest,
+				ImageMetadataProvider: tt.fields.ImageMetadataProvider,
+				ImageFileCache:        tt.fields.ImageFileCache,
+				lock:                  tt.fields.lock,
+			}
+			got, err := p.GetPayload(tt.args.ctx, tt.args.releaseImage, tt.args.customConfig, tt.args.pullSecretHash, tt.args.additionalTrustBundleHash, tt.args.hcConfigurationHash)
+			if (err != nil) != tt.wantError {
+				t.Errorf("LocalIgnitionProvider.GetPayload() error = %v, wantErr %v", err, tt.wantError)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LocalIgnitionProvider.GetPayload() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func initSecretOrDie(namespace, name, key string, data []byte) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: map[string][]byte{
+			key: data,
+		},
+	}
+}
+
+func initconfigMapOrDie(namespace, name, key, data string) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: map[string]string{
+			key: data},
+	}
+}
+
+func TestLocalIgnitionProvider_GetPayload(t *testing.T) {
+	type fields struct {
+		Client                client.Client
+		ReleaseProvider       releaseinfo.ProviderWithOpenShiftImageRegistryOverrides
+		CloudProvider         hyperv1.PlatformType
+		Namespace             string
+		WorkDir               string
+		PreserveOutput        bool
+		FeatureGateManifest   string
+		ImageMetadataProvider *util.RegistryClientImageMetadataProvider
+		ImageFileCache        *imageFileCache
+		lock                  sync.Mutex
+	}
+	type args struct {
+		ctx                       context.Context
+		releaseImage              string
+		customConfig              string
+		pullSecretHash            string
+		additionalTrustBundleHash string
+		hcConfigurationHash       string
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		want      []byte
+		objects   []client.Object
+		wantError bool
+	}{
+		{name: "WIP:",
+			fields: fields{
+				Namespace: "the-namespace",
+				lock:      sync.Mutex{},
+			},
+			args: args{
+				ctx: context.TODO(),
+			},
+			want: nil,
+			objects: []client.Object{
+				initSecretOrDie("the-namespace", pullSecretName, corev1.DockerConfigJsonKey, []byte("data")),
+				initconfigMapOrDie("the-namespace", additionalTrustBundleName, "ca-bundle.crt", "data"),
+				initSecretOrDie("the-namespace", "bootstrap-kubeconfig", "kubeconfig", []byte("data")),
+			},
+			wantError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		client := fake.NewClientBuilder().
+			WithObjects(tt.objects...).
+			Build()
+		t.Run(tt.name, func(t *testing.T) {
+			p := &LocalIgnitionProvider{
+				Client:                client,
+				ReleaseProvider:       tt.fields.ReleaseProvider,
+				CloudProvider:         tt.fields.CloudProvider,
+				Namespace:             tt.fields.Namespace,
+				WorkDir:               tt.fields.WorkDir,
+				PreserveOutput:        tt.fields.PreserveOutput,
+				FeatureGateManifest:   tt.fields.FeatureGateManifest,
+				ImageMetadataProvider: tt.fields.ImageMetadataProvider,
+				ImageFileCache:        tt.fields.ImageFileCache,
+				lock:                  tt.fields.lock,
+			}
+			got, err := p.GetPayload(tt.args.ctx, tt.args.releaseImage, tt.args.customConfig, tt.args.pullSecretHash, tt.args.additionalTrustBundleHash, tt.args.hcConfigurationHash)
+			if (err != nil) != tt.wantError {
+				t.Errorf("LocalIgnitionProvider.GetPayload() error = %v, wantErr %v", err, tt.wantError)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LocalIgnitionProvider.GetPayload() = %v, want %v", got, tt.want)
 			}
 		})
 	}
